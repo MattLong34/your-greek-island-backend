22df65db71c52bf7dbee4174897c28c6e60ee253
MattLong34 <mdlong34@gmail.com>
2020-07-27 15:05:57 -0600

    added first half of seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,106 @@
 Island.destroy_all
 
-santorini = Island.create(name: "Santorini", 
-    image: "https://designspell.files.wordpress.com/2013/07/999015_10151664509541726_476588225_n.jpg",
-    main_page: true,
-    short_bio: "Santorini is a popular holiday destination in Greece, and part of the Cycladic island group in the Aegean Sea. It is best known for its picturesque clifftop tourist towns (Fira, Oia, and Imerovigli), all of which offer spectacular views over the caldera: the crater created after a 16th-century BC volcanic eruption. With a land area of 76 square kilometers, Santorini is a little bigger than New York’s island of Manhattan.",
-    long_bio: "This romantic island needs no introduction and is a popular location for couples' escapes, honeymoons and weddings abroad. Santorini, one of the Cyclade Islands, owes its dramatic scenery to a volcano which erupted in the 16th century. Here, you can watch some of the world's best sunsets, bask on beaches with red, black and green sand, and take in the contrasts of white Cycladic architecture against the deep blue of the Aegean Sea.", 
-    tourist_rank: 5, 
+corfu = Island.create(name: "Corfu", 
+    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/vlacherna-monastery-rat-island-on-background-corfu-royalty-free-image-1582042039.jpg?crop=1xw:0.91602xh;center,top&resize=980:*g",
+    main_page: false,
+    short_bio: "Influenced by past Venetian, French and British rule, the Ionian island of Corfu is packed with history.",
+    long_bio: "This romantic island needs no introduction and is a popular location for couples' escapes, honeymoons and weddInfluenced by past Venetian, French and British rule, the Ionian island of Corfu is packed with history, while offering some of the most beautiful beaches in Greece (we're looking at you, Paleokastritsa). It's also where the famous Durrell family lived for four years and the setting of ITV hit The Durrells, which takes viewers to attractive locations, from Mouse Island to Corfu Town, on one of the best Greek islands.", 
+    tourist_rank: 0, 
     family_rank: 0,
     budget_rank: 0
 )
 
-# mykonos = Island.create(name: "Mykonos",
-#     tourist_rank: 5,
-#     description: "Mykonos is a popular Greek island destination in the Aegean Sea, and one of our absolute favorite places to go in Greece. Part of the Cyclades chain, Mykonos is best known for its wonderful beaches, luxury Mykonos Town shopping, charming whitewashed buildings, and vibrant beach and nightclub scene.",
-#     image: "https://d24ndt2yiijez0.cloudfront.net/uploads/image/asset/7188/mykonos-landscapes-2.jpg"
-# )
+santorini = Island.create(name: "Santorini", 
+    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/blue-sea-in-summer-greek-islands-santorini-royalty-free-image-1582045104.jpg?crop=1.00xw:0.918xh;0,0.0816xh&resize=980:*",
+    main_page: true,
+    short_bio: "This romantic island needs no introduction and is a popular location for couples' escapes, honeymoons and weddings abroad.",
+    long_bio: "This romantic island needs no introduction and is a popular location for couples' escapes, honeymoons and weddings abroad. Santorini, one of the Cyclade Islands, owes its dramatic scenery to a volcano which erupted in the 16th century. Here, you can watch some of the world's best sunsets, bask on beaches with red, black and green sand, and take in the contrasts of white Cycladic architecture against the deep blue of the Aegean Sea.", 
+    tourist_rank: 0, 
+    family_rank: 0,
+    budget_rank: 0
+)
 
-# naxos = Island.create(name: "Naxos",
-#     tourist_rank: 4,
-#     family_friendly: true,
-#     description: "Naxos is the most family-friendly Greek Island. Great beaches, good food, fun vibe, and lots of small friendly hotels.",
-#     image: "https://www.birikos.com/images/pics/sq-agios-prokopios-naxos.jpg"
-# )
+kefalonia = Island.create(name: "Kefalonia", 
+    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/scenic-view-of-sea-against-blue-sky-royalty-free-image-1582045312.jpg?crop=1.00xw:0.921xh;0,0.0612xh&resize=980:*",
+    main_page: false,
+    short_bio: "With its hidden beaches, laid-back atmosphere and stunning landscapes, Kefalonia is one of the best Greek islands for older couples and was the setting for Louis de Bernière's Captain Corelli's Mandolin.",
+    long_bio: "With its hidden beaches, laid-back atmosphere and stunning landscapes, Kefalonia is one of the best Greek islands for older couples and was the setting for Louis de Bernière's Captain Corelli's Mandolin. It's calm, quiet and a great place to get active with hiking, kayaking and experience a spot of wellness too. Fragrant pine, cypress and olive trees make getting out and about a treat, and if you head into Argostoli after dark, you might spot locals singing to the sounds of a mandolin.", 
+    tourist_rank: 0, 
+    family_rank: 0,
+    budget_rank: 0
+)
 
-# paros = Island.create(name: "Paros",
-#     tourist_rank: 4,
-#     description: "Paros, Greece, is a beautiful Cycladic island only 2 hours by ferry from both Santorini and Mykonos. Paros is best known for its great beaches, lively nightlife, and wonderful white washed towns. Taking a ferry from Paros is one of the few ways to access Antiparos, a smaller and more secluded island located just west of Paros with great beaches, excellent food, and charming family-owned hotels.",
-#     image: "https://i.pinimg.com/736x/33/ff/14/33ff148c004b2b8c12f79891bff51b6e--white-houses-paros.jpg"
-# )
+crete = Island.create(name: "Crete", 
+    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/beach-in-crete-greece-royalty-free-image-1582045525.jpg?crop=1xw:0.93387xh;center,top&resize=980:*",
+    main_page: false,
+    short_bio: "The biggest and best Greek island for those who like to escape for a fortnight, there's plenty to keep you entertained on Crete, whether it's exploring seductive beaches, tasting exquisite food or getting to know the picturesque towns and cities like Chania and Elounda.",
+    long_bio: "The biggest and best Greek island for those who like to escape for a fortnight, there's plenty to keep you entertained on Crete, whether it's exploring seductive beaches, tasting exquisite food or getting to know the picturesque towns and cities like Chania and Elounda. From feasting to sunbathing and hiking to discovering cultural sites like Rethymno's Fortezza and the palace of Knossos, there's something for everyone in Crete.", 
+    tourist_rank: 0, 
+    family_rank: 0,
+    budget_rank: 0
+)
 
-# crete = Island.create(name: "Crete",
-#     tourist_rank: 3,
-#     description: "The birthplace of both the king of the Olympian gods and of modern European civilization, Crete is a Mediterranean jewel. It’s rich with archaeological and mythological history that’s reflected in its ancient ruins and cultural attractions.",
-#     image: "http://1024x.net/wallpapers/60s/Agios_Nikolaos_Crete_Greece.jpg"
-# )
+rhodes = Island.create(name: "Rhodes", 
+    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lindos-with-beach-acropolis-royalty-free-image-1582045651.jpg?crop=1.00xw:0.918xh;0,0.0357xh&resize=980:*",
+    main_page: false,
+    short_bio: "For timeless beauty and impressive history, Rhodes is the island to check out.",
+    long_bio: "For timeless beauty and impressive history, Rhodes is the island to check out. Stroll the medieval Old Town, marvel at Mandraki Harbour and be wowed by the Acropolis of Lindos as you soak up the ancient history on one of the best Greek islands. This is where the Byzantines, Greeks, Venetian and Turks all left their marks and it's not just about centuries-old ruins as you'll find sandy beaches, traditional tavernas and green attractions, such as the Valley of Butterflies.", 
+    tourist_rank: 0, 
+    family_rank: 0,
+    budget_rank: 0
+)
+
+kastellorizo = Island.create(name: "Kastellorizo", 
+    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/kastellorizo-island-greece-royalty-free-image-1582045815.jpg?crop=1.00xw:0.935xh;0,0.0494xh&resize=980:*",
+    main_page: false,
+    short_bio: "A small, pretty island in the Dodecanese, Kastellorizo is in the easternmost tip of Greece and very close to Turkey, offering unique architecture and colourful houses in its main town.",
+    long_bio: "A small, pretty island in the Dodecanese, Kastellorizo is in the easternmost tip of Greece and very close to Turkey, offering unique architecture and colourful houses in its main town. You'll find a cluster of islets and plenty of marine life to discover in its clear waters.
+
+    If you're not too fussed about spending time on fine-sand beaches and would rather experience an authentic Greek island with friendly locals, this is the one for you. Around the island, you'll find a medieval castle, archaeological museums and lovely tavernas.", 
+    tourist_rank: 0, 
+    family_rank: 0,
+    budget_rank: 0
+)
+
+chios = Island.create(name: "Chios", 
+    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/pyrgi-chios-island-greece-royalty-free-image-1582045989.jpg?crop=1.00xw:0.918xh;0,0.0357xh&resize=980:*",
+    main_page: false,
+    short_bio: "Chios is a delight and best known for producing mastic, an aromatic resin that comes from trees grown in the Mastihohoria region and forms teardrop shapes.",
+    long_bio: "Chios is a delight and best known for producing mastic, an aromatic resin that comes from trees grown in the Mastihohoria region and forms teardrop shapes. While here, you'll want to check out the beautiful village of Pyrgi and see its houses decorated with grey and white geometric shapes. A relatively unexplored isle but one of the best Greek islands to visit for nature, mosaics and excellent food (try the cheeses, Chios mandarins and fresh fish).", 
+    tourist_rank: 0, 
+    family_rank: 0,
+    budget_rank: 0
+)
+
+hydra = Island.create(name: "Hydra", 
+    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/panorama-of-fishing-boats-on-hydra-island-greece-royalty-free-image-1582046269.jpg?crop=1.00xw:0.921xh;0,0.0332xh&resize=980:*",
+    main_page: false,
+    short_bio: "Another of the best Greek islands for couples, Hydra is a romantic spot you can easily reach from Athens.",
+    long_bio: "Another of the best Greek islands for couples, Hydra is a romantic spot you can easily reach from Athens. It's part of the Saronic Islands, it's home to traditional stone houses, narrow cobbled streets and secluded squares. People get around by donkey and the main hub is the waterfront, with its pretty streets, museums, churches and cafés. Hydra is also a popular day trip destination from, where you can relax on its beaches like Limnioniza and Spiliá.", 
+    tourist_rank: 0, 
+    family_rank: 0,
+    budget_rank: 0
+)
+
+skopelos = Island.create(name: "Skopelos", 
+    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/traditional-narrow-streets-with-cute-cafe-bars-in-royalty-free-image-1582046437.jpg?crop=0.98739xw:1xh;center,top&resize=980:*",
+    main_page: false,
+    short_bio: "The setting for Mamma Mia! and possibly the most beautiful Greek island is Skopelos.",
+    long_bio: "The setting for Mamma Mia! and possibly the most beautiful Greek island is Skopelos. With its tree-studded beaches, hidden coves and tiny chapels, it's not hard to see why this isle was the inspiration for the hit film. Here, you can truly relax, stroll through the picturesque main town and immerse yourself in the arty corners, with hand-crafted plates and vases you'll want to take home.", 
+    tourist_rank: 0, 
+    family_rank: 0,
+    budget_rank: 0
+)
+
+kos = Island.create(name: "Kos", 
+    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/scenic-view-of-sea-against-clear-blue-sky-royalty-free-image-1582048308.jpg?crop=1.00xw:0.921xh;0,0.0793xh&resize=980:*",
+    main_page: false,
+    short_bio: "Its turquoise waters, ancient sites and sprawling beaches place Kos firmly on our list of the best Greek islands.",
+    long_bio: "Its turquoise waters, ancient sites and sprawling beaches place Kos firmly on our list of the best Greek islands. This is the isle if you're after something more than beaches as it comes with rich history and an abundance of wellness spots. It's home to one of Greece's most important archaeological sites, Hippocrates' Asclepeion, which was a healing centre containing temples, hot springs and a school for physicians. You can experience Kos' thermal springs yourself at Agios Fokas, Piso Thermes and Kokkinonero.", 
+    tourist_rank: 0, 
+    family_rank: 0,
+    budget_rank: 0
+)
 
 # Matt: 1-10
 # Hailey: 11-20

4903b4bc7081916229c6e116bf13c06e497462f2
haileyringier <haileyringier@outlook.com>
2020-07-27 15:12:29 -0600

    adds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,103 +1,11 @@
 Island.destroy_all
 
-corfu = Island.create(name: "Corfu", 
-    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/vlacherna-monastery-rat-island-on-background-corfu-royalty-free-image-1582042039.jpg?crop=1xw:0.91602xh;center,top&resize=980:*g",
-    main_page: false,
-    short_bio: "Influenced by past Venetian, French and British rule, the Ionian island of Corfu is packed with history.",
-    long_bio: "This romantic island needs no introduction and is a popular location for couples' escapes, honeymoons and weddInfluenced by past Venetian, French and British rule, the Ionian island of Corfu is packed with history, while offering some of the most beautiful beaches in Greece (we're looking at you, Paleokastritsa). It's also where the famous Durrell family lived for four years and the setting of ITV hit The Durrells, which takes viewers to attractive locations, from Mouse Island to Corfu Town, on one of the best Greek islands.", 
-    tourist_rank: 0, 
-    family_rank: 0,
-    budget_rank: 0
-)
-
 santorini = Island.create(name: "Santorini", 
-    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/blue-sea-in-summer-greek-islands-santorini-royalty-free-image-1582045104.jpg?crop=1.00xw:0.918xh;0,0.0816xh&resize=980:*",
+    image: "https://designspell.files.wordpress.com/2013/07/999015_10151664509541726_476588225_n.jpg",
     main_page: true,
-    short_bio: "This romantic island needs no introduction and is a popular location for couples' escapes, honeymoons and weddings abroad.",
+    short_bio: "Santorini is a popular holiday destination in Greece, and part of the Cycladic island group in the Aegean Sea. It is best known for its picturesque clifftop tourist towns (Fira, Oia, and Imerovigli), all of which offer spectacular views over the caldera: the crater created after a 16th-century BC volcanic eruption. With a land area of 76 square kilometers, Santorini is a little bigger than New York’s island of Manhattan.",
     long_bio: "This romantic island needs no introduction and is a popular location for couples' escapes, honeymoons and weddings abroad. Santorini, one of the Cyclade Islands, owes its dramatic scenery to a volcano which erupted in the 16th century. Here, you can watch some of the world's best sunsets, bask on beaches with red, black and green sand, and take in the contrasts of white Cycladic architecture against the deep blue of the Aegean Sea.", 
-    tourist_rank: 0, 
-    family_rank: 0,
-    budget_rank: 0
-)
-
-kefalonia = Island.create(name: "Kefalonia", 
-    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/scenic-view-of-sea-against-blue-sky-royalty-free-image-1582045312.jpg?crop=1.00xw:0.921xh;0,0.0612xh&resize=980:*",
-    main_page: false,
-    short_bio: "With its hidden beaches, laid-back atmosphere and stunning landscapes, Kefalonia is one of the best Greek islands for older couples and was the setting for Louis de Bernière's Captain Corelli's Mandolin.",
-    long_bio: "With its hidden beaches, laid-back atmosphere and stunning landscapes, Kefalonia is one of the best Greek islands for older couples and was the setting for Louis de Bernière's Captain Corelli's Mandolin. It's calm, quiet and a great place to get active with hiking, kayaking and experience a spot of wellness too. Fragrant pine, cypress and olive trees make getting out and about a treat, and if you head into Argostoli after dark, you might spot locals singing to the sounds of a mandolin.", 
-    tourist_rank: 0, 
-    family_rank: 0,
-    budget_rank: 0
-)
-
-crete = Island.create(name: "Crete", 
-    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/beach-in-crete-greece-royalty-free-image-1582045525.jpg?crop=1xw:0.93387xh;center,top&resize=980:*",
-    main_page: false,
-    short_bio: "The biggest and best Greek island for those who like to escape for a fortnight, there's plenty to keep you entertained on Crete, whether it's exploring seductive beaches, tasting exquisite food or getting to know the picturesque towns and cities like Chania and Elounda.",
-    long_bio: "The biggest and best Greek island for those who like to escape for a fortnight, there's plenty to keep you entertained on Crete, whether it's exploring seductive beaches, tasting exquisite food or getting to know the picturesque towns and cities like Chania and Elounda. From feasting to sunbathing and hiking to discovering cultural sites like Rethymno's Fortezza and the palace of Knossos, there's something for everyone in Crete.", 
-    tourist_rank: 0, 
-    family_rank: 0,
-    budget_rank: 0
-)
-
-rhodes = Island.create(name: "Rhodes", 
-    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lindos-with-beach-acropolis-royalty-free-image-1582045651.jpg?crop=1.00xw:0.918xh;0,0.0357xh&resize=980:*",
-    main_page: false,
-    short_bio: "For timeless beauty and impressive history, Rhodes is the island to check out.",
-    long_bio: "For timeless beauty and impressive history, Rhodes is the island to check out. Stroll the medieval Old Town, marvel at Mandraki Harbour and be wowed by the Acropolis of Lindos as you soak up the ancient history on one of the best Greek islands. This is where the Byzantines, Greeks, Venetian and Turks all left their marks and it's not just about centuries-old ruins as you'll find sandy beaches, traditional tavernas and green attractions, such as the Valley of Butterflies.", 
-    tourist_rank: 0, 
-    family_rank: 0,
-    budget_rank: 0
-)
-
-kastellorizo = Island.create(name: "Kastellorizo", 
-    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/kastellorizo-island-greece-royalty-free-image-1582045815.jpg?crop=1.00xw:0.935xh;0,0.0494xh&resize=980:*",
-    main_page: false,
-    short_bio: "A small, pretty island in the Dodecanese, Kastellorizo is in the easternmost tip of Greece and very close to Turkey, offering unique architecture and colourful houses in its main town.",
-    long_bio: "A small, pretty island in the Dodecanese, Kastellorizo is in the easternmost tip of Greece and very close to Turkey, offering unique architecture and colourful houses in its main town. You'll find a cluster of islets and plenty of marine life to discover in its clear waters.
-
-    If you're not too fussed about spending time on fine-sand beaches and would rather experience an authentic Greek island with friendly locals, this is the one for you. Around the island, you'll find a medieval castle, archaeological museums and lovely tavernas.", 
-    tourist_rank: 0, 
-    family_rank: 0,
-    budget_rank: 0
-)
-
-chios = Island.create(name: "Chios", 
-    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/pyrgi-chios-island-greece-royalty-free-image-1582045989.jpg?crop=1.00xw:0.918xh;0,0.0357xh&resize=980:*",
-    main_page: false,
-    short_bio: "Chios is a delight and best known for producing mastic, an aromatic resin that comes from trees grown in the Mastihohoria region and forms teardrop shapes.",
-    long_bio: "Chios is a delight and best known for producing mastic, an aromatic resin that comes from trees grown in the Mastihohoria region and forms teardrop shapes. While here, you'll want to check out the beautiful village of Pyrgi and see its houses decorated with grey and white geometric shapes. A relatively unexplored isle but one of the best Greek islands to visit for nature, mosaics and excellent food (try the cheeses, Chios mandarins and fresh fish).", 
-    tourist_rank: 0, 
-    family_rank: 0,
-    budget_rank: 0
-)
-
-hydra = Island.create(name: "Hydra", 
-    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/panorama-of-fishing-boats-on-hydra-island-greece-royalty-free-image-1582046269.jpg?crop=1.00xw:0.921xh;0,0.0332xh&resize=980:*",
-    main_page: false,
-    short_bio: "Another of the best Greek islands for couples, Hydra is a romantic spot you can easily reach from Athens.",
-    long_bio: "Another of the best Greek islands for couples, Hydra is a romantic spot you can easily reach from Athens. It's part of the Saronic Islands, it's home to traditional stone houses, narrow cobbled streets and secluded squares. People get around by donkey and the main hub is the waterfront, with its pretty streets, museums, churches and cafés. Hydra is also a popular day trip destination from, where you can relax on its beaches like Limnioniza and Spiliá.", 
-    tourist_rank: 0, 
-    family_rank: 0,
-    budget_rank: 0
-)
-
-skopelos = Island.create(name: "Skopelos", 
-    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/traditional-narrow-streets-with-cute-cafe-bars-in-royalty-free-image-1582046437.jpg?crop=0.98739xw:1xh;center,top&resize=980:*",
-    main_page: false,
-    short_bio: "The setting for Mamma Mia! and possibly the most beautiful Greek island is Skopelos.",
-    long_bio: "The setting for Mamma Mia! and possibly the most beautiful Greek island is Skopelos. With its tree-studded beaches, hidden coves and tiny chapels, it's not hard to see why this isle was the inspiration for the hit film. Here, you can truly relax, stroll through the picturesque main town and immerse yourself in the arty corners, with hand-crafted plates and vases you'll want to take home.", 
-    tourist_rank: 0, 
-    family_rank: 0,
-    budget_rank: 0
-)
-
-kos = Island.create(name: "Kos", 
-    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/scenic-view-of-sea-against-clear-blue-sky-royalty-free-image-1582048308.jpg?crop=1.00xw:0.921xh;0,0.0793xh&resize=980:*",
-    main_page: false,
-    short_bio: "Its turquoise waters, ancient sites and sprawling beaches place Kos firmly on our list of the best Greek islands.",
-    long_bio: "Its turquoise waters, ancient sites and sprawling beaches place Kos firmly on our list of the best Greek islands. This is the isle if you're after something more than beaches as it comes with rich history and an abundance of wellness spots. It's home to one of Greece's most important archaeological sites, Hippocrates' Asclepeion, which was a healing centre containing temples, hot springs and a school for physicians. You can experience Kos' thermal springs yourself at Agios Fokas, Piso Thermes and Kokkinonero.", 
-    tourist_rank: 0, 
+    tourist_rank: 5, 
     family_rank: 0,
     budget_rank: 0
 )
@@ -107,7 +15,7 @@ kos = Island.create(name: "Kos",
 # https://www.prima.co.uk/travel/g30976502/best-greek-islands/https://www.prima.co.uk/travel/g30976502/best-greek-islands/https://www.prima.co.uk/travel/g30976502/best-greek-islands/
 
 mykonos = Island.create(name: "Mykonos", 
-    image: "",
+    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/suspending-boat-royalty-free-image-1582107742.jpg?crop=1xw:0.91279xh;center,top&resize=980:*",
     main_page: false,
     short_bio: "Best Greek island for the style-conscious",
     long_bio: "An island for those who like to be seen, Mykonos is not only the most glamorous Greek island, but one of the most stylish in the world. Here, you can stay in chic hotels, experience world-class nightlife and rub shoulders with the trendiest jet-setters around.",
@@ -117,7 +25,7 @@ mykonos = Island.create(name: "Mykonos",
 )
 
 zakynthos = Island.create(name: "Zakynthos(Zante)", 
-    image: "",
+    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/suspending-boat-royalty-free-image-1582107742.jpg?crop=1xw:0.91279xh;center,top&resize=980:*",
     main_page: false,
     short_bio: "Best Greek island for spotting sea turtles",
     long_bio: "Zakynthos, or Zante as it is also known, isn't just home to drop-dead gorgeous beaches like Shipwreck Beach, but is known for its endangered sea turtles, which lay their eggs on the Ionian island's white sands.",
@@ -126,7 +34,7 @@ zakynthos = Island.create(name: "Zakynthos(Zante)",
     budget_rank: 0
 )
 tinos = Island.create(name: "Tinos", 
-    image: "",
+    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/suspending-boat-royalty-free-image-1582107742.jpg?crop=1xw:0.91279xh;center,top&resize=980:*",
     main_page: false,
     short_bio: "Best Greek island for incredible scenery",
     long_bio: "Set in the Cyclades, Tinos is the place to visit if you appreciate wild beauty, typical Greek villages that climb up the side of mountains and lunar landscapes. There are 750 chapels, Tinian cheese to try and the village gems to visit include Pyrgos, Ktikado, Falatados and Tripotamos",
@@ -135,7 +43,7 @@ tinos = Island.create(name: "Tinos",
     budget_rank: 0
 )
 skiathos = Island.create(name: "Skiathos", 
-    image: "",
+    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/suspending-boat-royalty-free-image-1582107742.jpg?crop=1xw:0.91279xh;center,top&resize=980:*",
     main_page: false,
     short_bio: "Best Greek island for beaches",
     long_bio: "With 60 splendid beaches to choose from, it's safe to say that Skiathos is the best Greek island for sunbathers. From Lalaria to Koukounaries, you'll find lovely stretches of sand and pebble beaches backed by lush pine trees. ",
@@ -144,7 +52,7 @@ skiathos = Island.create(name: "Skiathos",
     budget_rank: 0
 )
 symi = Island.create(name: "Symi", 
-    image: "",
+    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/suspending-boat-royalty-free-image-1582107742.jpg?crop=1xw:0.91279xh;center,top&resize=980:*",
     main_page: false,
     short_bio: "Best Greek island for architecture",
     long_bio: "A picturesque island boasting one of the most beautiful harbours in Greece, Symi also treats visitors to stunning architecture, great cafes and restaurants, plus a slower pace of life. ",
@@ -153,7 +61,7 @@ symi = Island.create(name: "Symi",
     budget_rank: 0
 )
 lefkada = Island.create(name: "Lefkada", 
-    image: "",
+    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/suspending-boat-royalty-free-image-1582107742.jpg?crop=1xw:0.91279xh;center,top&resize=980:*",
     main_page: false,
     short_bio: "Best Greek island for watersports",
     long_bio: "This Ionian gem is easy to access, friendly and – all the ingredients you need for a fun holiday on one of the best Greek islands for active travellers. A number of its beaches have been awarded Blue Flags and Lefkada, or Lefkas, is the place to sail, windsurf and enjoy other watersports.",
@@ -162,7 +70,7 @@ lefkada = Island.create(name: "Lefkada",
     budget_rank: 0
 )
 alonnisos = Island.create(name: "Alonnisos", 
-    image: "",
+    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/suspending-boat-royalty-free-image-1582107742.jpg?crop=1xw:0.91279xh;center,top&resize=980:*",
     main_page: false,
     short_bio: "Best Greek island for nature lovers",
     long_bio: "This island treasure offers rugged landscapes, olive groves and pine forests perfect for leisurely walks – it's a nature lover's dream! The most remote of the Northern Sporades, Alonissos, is home to the National Marine Park of Northern Sporades, where rare seabirds, dolphins and the Mediterranean monk seal reside.",
@@ -171,7 +79,7 @@ alonnisos = Island.create(name: "Alonnisos",
     budget_rank: 0
 )
 sifnos = Island.create(name: "Sifanos", 
-    image: "",
+    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/astypalea-island-dodecanese-greece-royalty-free-image-1582108014.jpg?crop=0.896xw:1.00xh;0.104xw,0&resize=980:*",
     main_page: false,
     short_bio: "Best Greek island for foodies",
     long_bio: "An island of flavours, Sifnos is where you'll discover mouth-watering local specialties, such as chickpea balls, mastelo (lamb cooked in red wine and spices) and honey pies. One of the best Greek islands for its traditional villages and secluded beaches, it capital Apollonia is known for pretty narrow streets, white-washed houses and the Folklore Museum. ",
@@ -180,7 +88,7 @@ sifnos = Island.create(name: "Sifanos",
     budget_rank: 0
 )
 astypalaia = Island.create(name: "Astypalaia", 
-    image: "",
+    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/poros-greece-royalty-free-image-1582108299.jpg?crop=1.00xw:0.918xh;0,0.0128xh&resize=980:*",
     main_page: false,
     short_bio: "Best Greek island for going off the beaten track",
     long_bio: "This Dodecanese island shaped like a butterfly sits far west of Kos and has centuries-old history, attractive villages and sun-drenched beaches that many tourists fail to explore.",
@@ -194,6 +102,6 @@ poros = Island.create(name: "Poross",
     short_bio: "Best Greek island from Athens",
     long_bio: "This wonderful Greek island is a popular weekend spot among Athenians and a fabulous place to escape. The small island of Poros is actually made up of two islands – Spheria and Kalavria – which are connected by a road. You'll find everything you need from one of the best Greek islands – beaches, natural scenery and lots of sunshine that couples and families will love.",
     tourist_rank: 0, 
-    family_rank: 0,
+    family_rank: 5,
     budget_rank: 0
 )
